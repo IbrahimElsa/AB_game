2058246ab241a2c657b3010d7d29e1ae718781f1
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-04-28 16:36:34 -0400

    db change final hopefully :3

--- AB_game/obj/Debug/net8.0-windows/AB_game.AssemblyInfo.cs
+++ AB_game/obj/Debug/net8.0-windows/AB_game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AB_game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d15739bf14bf2347777e13ee113335cb42a8f3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a6db636c0348517b1862732f373ecc933dd6b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AB_game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AB_game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
